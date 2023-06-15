a9f4222da18b590cfbe2a2483350b2ad5eae9e09
Влад Кот <vanderofcat@gmail.com>
2023-06-15 12:55:46 +0300

    unhardcode json name + custom output folder

--- ponyairedub.py
+++ ponyairedub.py
@@ -7,6 +7,7 @@ import os
 import librosa
 import numpy as np
 import soundfile
+import sys
 import torch
 import json
 from pydub import AudioSegment
@@ -80,8 +81,11 @@ class ModelDefenition:
 
 print("loading pony.json")
 pony = {}
-with open("./pony.json", "r") as f:
+with open(sys.argv[1] if len(sys.argv)>1 else "./pony.json", "r") as f:
     pony = json.loads(f.read())
+
+if not os.path.isdir(pony["outputFolder"]):
+    os.mkdir(pony["outputFolder"])
 
 print("loading character map")
 characterMap = {}
@@ -166,7 +170,7 @@ for name, modelInfo in pony["characterModels"].items():
             print ("processing track "+str(i))
             finalAudio+=characterAudio[characterAudioMap[i][2]*1000:characterAudioMap[i][3]*1000]
             finalAudio+=AudioSegment.silent(duration=(characterAudioMap[i+1][0]-characterAudioMap[i][1])*1000) if i+1 < len(characterAudioMap) else AudioSegment.empty()
-        finalAudio.export("./result/"+name+".wav", format="wav")
+        finalAudio.export(Path(pony["outputFolder"],name+".wav"), format="wav")
 
     finally:
         del svc_model
